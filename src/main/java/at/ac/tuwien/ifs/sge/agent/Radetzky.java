1de413b6076961fb499fad89ca9fb724824065a8
Peter Kabelik <>
2024-01-20 09:00:58 +0100

    Implement random unit movement

--- src/main/java/at/ac/tuwien/ifs/sge/agent/Radetzky.java
+++ src/main/java/at/ac/tuwien/ifs/sge/agent/Radetzky.java
@@ -5,23 +5,30 @@ import at.ac.tuwien.ifs.sge.core.engine.communication.ActionResult;
 import at.ac.tuwien.ifs.sge.core.game.exception.ActionException;
 import at.ac.tuwien.ifs.sge.core.util.Util;
 import at.ac.tuwien.ifs.sge.game.empire.communication.event.EmpireEvent;
+import at.ac.tuwien.ifs.sge.game.empire.communication.event.action.MovementAction;
+import at.ac.tuwien.ifs.sge.game.empire.communication.event.order.start.MovementStartOrder;
 import at.ac.tuwien.ifs.sge.game.empire.core.Empire;
+import at.ac.tuwien.ifs.sge.game.empire.exception.EmpireMapException;
+import at.ac.tuwien.ifs.sge.game.empire.model.units.EmpireUnit;
+import at.ac.tuwien.ifs.sge.game.empire.model.units.EmpireUnitState;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.UUID;
 
 public class Radetzky extends AbstractRealTimeGameAgent<Empire, EmpireEvent> {
     private static final int START_DELAY_IN_MILLISECONDS = 1000;
     private static final int TIMER_INTERVAL_IN_MILLISECONDS = 100;
     private static final int ACTION_EXECUTION_TIME_IN_MILLISECONDS = 100;
 
-    private final Radetzky radetzky = this;
     private final Random random = new Random();
-    private final Timer timer = new Timer();
 
-    private boolean actionInProgress = false;
+    private Map<UUID, EmpireUnitState> unitStates = new HashMap<>();
 
     public static void main(String[] args) {
         var playerId = getPlayerIdFromArgs(args);
@@ -37,46 +44,76 @@ public class Radetzky extends AbstractRealTimeGameAgent<Empire, EmpireEvent> {
 
     @Override
     public void startPlaying() {
-        TimerTask task = new TimerTask() {
-            @Override
-            public void run() {
-                if (!actionInProgress) {
-                    var actions = getGame().getPossibleActions(playerId);
+        var units = getGame().getUnitsByPlayer(playerId);
 
-                    log.debug("Possible actions: " + actions.size() + ":");
+        for (var unit : units) {
+            unitStates.put(unit.getId(), EmpireUnitState.Idle);
 
-                    for (var action : actions) {
-                        log.debug("\t" + action.toString());
-                    }
-
-                    var nextAction = Util.selectRandom(actions, random);
-
-                    radetzky.sendAction(nextAction, System.currentTimeMillis() + ACTION_EXECUTION_TIME_IN_MILLISECONDS);
-
-                    log.debug("Next Action:\n\t" + nextAction.toString());
-
-                    actionInProgress = true;
-                }
-            }
-        };
-
-        timer.schedule(task, START_DELAY_IN_MILLISECONDS, TIMER_INTERVAL_IN_MILLISECONDS);
+            employUnit(unit);
+        }
     }
 
     @Override
     protected void onGameUpdate(HashMap<EmpireEvent, ActionResult> actionsWithResult) {
-        log.debug("onGameUpdate");
-        actionInProgress = false;
+        for (var entry : actionsWithResult.entrySet()) {
+            handleOnGameUpdateAction(entry.getKey(), entry.getValue());
+        }
+    }
+
+    private void handleOnGameUpdateAction(EmpireEvent action, ActionResult result) {
+        log.debug("Action " + action.toString() + " ACCEPTED.");
+
+        if (action instanceof MovementAction movementAction) {
+            var unitId = movementAction.getUnitId();
+
+            unitStates.put(unitId, EmpireUnitState.Idle);
+
+            employUnit(getGame().getUnit(unitId));
+        }
     }
 
     @Override
     protected void onActionRejected(EmpireEvent action) {
-        log.debug("onActionRejected");
-        actionInProgress = false;
+        log.debug("Action " + action.toString() + " REJECTED.");
+
+        if (action instanceof MovementStartOrder movementStartOrder) {
+            var unitId = movementStartOrder.getUnitId();
+
+            unitStates.put(unitId, EmpireUnitState.Idle);
+
+            employUnit(getGame().getUnit(unitId));
+        }
+    }
+
+    private void employUnit(EmpireUnit unit) {
+        if (unit == null) {
+            return;
+        }
+
+        if (unitStates.getOrDefault(unit.getId(), EmpireUnitState.Idle) != EmpireUnitState.Idle) {
+            return;
+        }
+
+        try {
+            var possibleUnitActions = getGame().getBoard().getPossibleActions(unit);
+            var possibleUnitMoveActions = new ArrayList<MovementStartOrder>();
+
+            for (var action : possibleUnitActions) {
+                if (action instanceof MovementStartOrder movementStartOrder) {
+                    possibleUnitMoveActions.add(movementStartOrder);
+                }
+            }
+
+            var nextAction = Util.selectRandom(possibleUnitMoveActions, random);
+
+            sendAction(nextAction, System.currentTimeMillis() + ACTION_EXECUTION_TIME_IN_MILLISECONDS);
+        } catch (EmpireMapException e) {
+            log.error("Actions for unit " + unit.getId() + " could not be retrieved.");
+            log.printStackTrace(e);
+        }
     }
 
     @Override
     public void shutdown() {
-        timer.cancel();
     }
 }
